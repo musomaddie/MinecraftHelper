60676ad271b2df7fda6733511499d5e5b86477f5
Madeleine Wagner <musomaddy@gmail.com>
2022-08-26 15:02:34 +1000

    Added first test!! :)

---
+++ test_block_adder_flask/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO item_to_group(item_name, item_group)
+VALUES ("Testing Item", "Testing Group");
---
+++ test_block_adder_flask/test_db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+import pytest
+
+from block_adder_flask.db_for_flask import get_db
+
+
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute("SELECT 1")
+
+    assert "closed" in str(e.value)
