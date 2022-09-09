1d997dc9bb4bfc0f2390126c5b4ca01af3302622
Madeleine Wagner <musomaddy@gmail.com>
2022-09-10 09:28:04 +1000

    Added implementation to get obtainining methods as part of group

--- block_adder_flask/get_group_info.py
+++ block_adder_flask/get_group_info.py
@@ -6,12 +6,31 @@ from block_adder_flask.json_utils import get_file_contents, update_json_file
 JSON_DIR = "block_adder_flask/item_information"
 
 
-def get_updated_group_name(from_db: str, json_data: dict) -> str:
-    if "group" in json_data:
-        return json_data["group"]
-    if from_db is None:
-        return ""
-    return from_db
+def _get_different_item_from_group(group_name, current_item):
+    for item in get_file_contents(f"{JSON_DIR}/groups/{group_name}.json")["items"]:
+        if item != current_item:
+            return item
+
+
+def get_obtaining_methods_in_group(group_name, current_item):
+    other_item_data = get_file_contents(
+        f"{JSON_DIR}/{_get_different_item_from_group(group_name, current_item)}")
+    keys = list(other_item_data.keys())
+    keys.remove("name")
+    keys.remove("group")
+    return keys
+
+
+def remove_from_group(group_name, item_name):
+    if group_name == "" or group_name is None:
+        return
+    group_fn_full = f"{JSON_DIR}/groups/{group_name}.json"
+    existing_group_info = get_file_contents(group_fn_full)
+    existing_group_info["items"].remove(item_name)
+    if len(existing_group_info["items"]) == 0:
+        os.remove(group_fn_full)
+    else:
+        update_json_file(existing_group_info, group_fn_full)
 
 
 def save_to_group(group_name, item_name):
@@ -32,17 +51,13 @@ def save_to_group(group_name, item_name):
 def should_show_group_button(group_name, this_item_name):
     if group_name is None or group_name == "" or group_name == "None":
         return False
-    group_items = get_file_contents(f"{JSON_DIR}/{group_name}.json")["items"]
+    group_items = get_file_contents(f"{JSON_DIR}/groups/{group_name}.json")["items"]
     return len(group_items) > 1
 
 
-def remove_from_group(group_name, item_name):
-    if group_name == "" or group_name is None:
-        return
-    group_fn_full = f"{JSON_DIR}/groups/{group_name}.json"
-    existing_group_info = get_file_contents(group_fn_full)
-    existing_group_info["items"].remove(item_name)
-    if len(existing_group_info["items"]) == 0:
-        os.remove(group_fn_full)
-    else:
-        update_json_file(existing_group_info, group_fn_full)
+def get_updated_group_name(from_db: str, json_data: dict) -> str:
+    if "group" in json_data:
+        return json_data["group"]
+    if from_db is None:
+        return ""
+    return from_db
--- test_block_adder_flask/test_get_group_item.py
+++ test_block_adder_flask/test_get_group_item.py
@@ -8,17 +8,58 @@ ITEM_NAME = "Test Item"
 GROUP_NAME = "Test Group"
 EXPECTED_JSON_DIR = "block_adder_flask/item_information"
 FILE_LOC = "block_adder_flask.get_group_info"
+MOCK_ITEM_CONTENTS = {
+    "name": ITEM_NAME,
+    "group": GROUP_NAME,
+    "breaking": {"sub key 1": "data", "sub key 2": ""},
+    "crafting": {"more data"}
+}
 
 
-@pytest.mark.parametrize(
-    ("from_db", "json_data", "expected_name"),
-    [("DB Group", {}, "DB Group"),
-     ("", {"group": "Json Group"}, "Json Group"),
-     (None, {}, ""),
-     ("DB Group", {"group": "Json Group"}, "Json Group")])
-def test_get_updated_group_name(from_db, json_data, expected_name):
-    assert gg.get_updated_group_name(
-        from_db, json_data) == expected_name
+@pytest.mark.parametrize("group_items", [[ITEM_NAME, "Test Item 2"], ["Test Item 2", ITEM_NAME]])
+@patch(f"{FILE_LOC}.get_file_contents")
+def test_get_different_item_from_group(mock_get_file_contents, group_items):
+    mock_get_file_contents.return_value = {"group name": GROUP_NAME, "items": group_items}
+    assert gg._get_different_item_from_group(GROUP_NAME, ITEM_NAME) == "Test Item 2"
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+@patch(f"{FILE_LOC}._get_different_item_from_group")
+def test_get_obtaining_methods_in_group(mock_get_different_item, mock_get_file_contents):
+    mock_get_file_contents.return_value = MOCK_ITEM_CONTENTS
+    assert gg.get_obtaining_methods_in_group(GROUP_NAME, ITEM_NAME) == ["breaking", "crafting"]
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+@patch(f"{FILE_LOC}.update_json_file")
+def test_remove_from_group(mock_update_json_file, mock_get_file_contents):
+    mock_get_file_contents.return_value = {
+        "group name": GROUP_NAME, "items": ["Existing Item", ITEM_NAME]}
+    gg.remove_from_group(GROUP_NAME, ITEM_NAME)
+    mock_update_json_file.assert_called_once_with(
+        {"group name": GROUP_NAME, "items": ["Existing Item"]},
+        f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json")
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+@patch(f"{FILE_LOC}.os.remove")
+def test_remove_from_group_only_item(mock_remove, mock_get_file_contents):
+    mock_get_file_contents.return_value = {"group name": GROUP_NAME, "items": [ITEM_NAME]}
+    gg.remove_from_group(GROUP_NAME, ITEM_NAME)
+    mock_remove.assert_called_once_with(f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json")
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+def test_remove_from_group_no_group(mock_get_file_contents):
+    gg.remove_from_group("", ITEM_NAME)
+    mock_get_file_contents.assert_not_called()
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+def test_remove_from_group_none_group_name(mock_get_file_contents):
+    gg.remove_from_group(None, ITEM_NAME)
+    mock_get_file_contents.assert_not_called()
+
 
 @patch(f"{FILE_LOC}.isfile")
 @patch(f"{FILE_LOC}.update_json_file")
@@ -29,6 +70,7 @@ def test_save_to_group_new_group(mock_update_json_file, mock_isfile):
         {"group name": GROUP_NAME, "items": [ITEM_NAME]},
         f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json"
     )
+
 
 @patch(f"{FILE_LOC}.isfile")
 @patch(f"{FILE_LOC}.get_file_contents")
@@ -85,32 +127,12 @@ def test_should_show_group_button_true(mock_get_file_contents):
     assert gg.should_show_group_button(GROUP_NAME, ITEM_NAME)
 
 
-@patch(f"{FILE_LOC}.get_file_contents")
-@patch(f"{FILE_LOC}.update_json_file")
-def test_remove_from_group(mock_update_json_file, mock_get_file_contents):
-    mock_get_file_contents.return_value = {
-        "group name": GROUP_NAME, "items": ["Existing Item", ITEM_NAME]}
-    gg.remove_from_group(GROUP_NAME, ITEM_NAME)
-    mock_update_json_file.assert_called_once_with(
-        {"group name": GROUP_NAME, "items": ["Existing Item"]},
-        f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json")
-
-
-@patch(f"{FILE_LOC}.get_file_contents")
-@patch(f"{FILE_LOC}.os.remove")
-def test_remove_from_group_only_item(mock_remove, mock_get_file_contents):
-    mock_get_file_contents.return_value = {"group name": GROUP_NAME, "items": [ITEM_NAME]}
-    gg.remove_from_group(GROUP_NAME, ITEM_NAME)
-    mock_remove.assert_called_once_with(f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json")
-
-
-@patch(f"{FILE_LOC}.get_file_contents")
-def test_remove_from_group_no_group(mock_get_file_contents):
-    gg.remove_from_group("", ITEM_NAME)
-    mock_get_file_contents.assert_not_called()
-
-
-@patch(f"{FILE_LOC}.get_file_contents")
-def test_remove_from_group_none_group_name(mock_get_file_contents):
-    gg.remove_from_group(None, ITEM_NAME)
-    mock_get_file_contents.assert_not_called()
+@pytest.mark.parametrize(
+    ("from_db", "json_data", "expected_name"),
+    [("DB Group", {}, "DB Group"),
+     ("", {"group": "Json Group"}, "Json Group"),
+     (None, {}, ""),
+     ("DB Group", {"group": "Json Group"}, "Json Group")])
+def test_get_updated_group_name(from_db, json_data, expected_name):
+    assert gg.get_updated_group_name(
+        from_db, json_data) == expected_name
