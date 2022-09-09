c554a13039bade9009f687c4df4fd10a05b5b963
Madeleine Wagner <musomaddy@gmail.com>
2022-09-10 08:55:52 +1000

    Added implementation for get group info

--- block_adder_flask/get_group_info.py
+++ block_adder_flask/get_group_info.py
@@ -5,12 +5,6 @@ from block_adder_flask.json_utils import get_file_contents, update_json_file
 JSON_DIR = "block_adder_flask/item_information"
 
 
-def should_show_group_button(group_name, this_item_name):
-    if group_name is None or group_name == "" or group_name == "None":
-        return False
-    return True
-
-
 def get_updated_group_name(from_db: str, json_data: dict) -> str:
     if "group" in json_data:
         return json_data["group"]
@@ -32,3 +26,10 @@ def save_to_group(group_name, item_name):
     else:
         group_items = {"group name": group_name, "items": [item_name]}
         update_json_file(group_items, group_fn_full)
+
+
+def should_show_group_button(group_name, this_item_name):
+    if group_name is None or group_name == "" or group_name == "None":
+        return False
+    group_items = get_file_contents(f"{JSON_DIR}/{group_name}.json")["items"]
+    return len(group_items) > 1
--- test_block_adder_flask/test_get_group_item.py
+++ test_block_adder_flask/test_get_group_item.py
@@ -10,10 +10,15 @@ EXPECTED_JSON_DIR = "block_adder_flask/item_information"
 FILE_LOC = "block_adder_flask.get_group_info"
 
 
-@pytest.mark.parametrize("group_name", [None, "", "None"])
-def test_should_show_group_button_missing_group_name(group_name):
-    assert not gg.should_show_group_button(group_name, "")
-
+@pytest.mark.parametrize(
+    ("from_db", "json_data", "expected_name"),
+    [("DB Group", {}, "DB Group"),
+     ("", {"group": "Json Group"}, "Json Group"),
+     (None, {}, ""),
+     ("DB Group", {"group": "Json Group"}, "Json Group")])
+def test_get_updated_group_name(from_db, json_data, expected_name):
+    assert gg.get_updated_group_name(
+        from_db, json_data) == expected_name
 
 @patch(f"{FILE_LOC}.isfile")
 @patch(f"{FILE_LOC}.update_json_file")
@@ -24,7 +29,6 @@ def test_save_to_group_new_group(mock_update_json_file, mock_isfile):
         {"group name": GROUP_NAME, "items": [ITEM_NAME]},
         f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json"
     )
-
 
 @patch(f"{FILE_LOC}.isfile")
 @patch(f"{FILE_LOC}.get_file_contents")
@@ -64,12 +68,18 @@ def test_save_to_group_existing_group(mock_update_json_file, mock_get_file_conte
         f"{EXPECTED_JSON_DIR}/groups/{GROUP_NAME}.json")
 
 
-@pytest.mark.parametrize(
-    ("from_db", "json_data", "expected_name"),
-    [("DB Group", {}, "DB Group"),
-     ("", {"group": "Json Group"}, "Json Group"),
-     (None, {}, ""),
-     ("DB Group", {"group": "Json Group"}, "Json Group")])
-def test_get_updated_group_name(from_db, json_data, expected_name):
-    assert gg.get_updated_group_name(
-        from_db, json_data) == expected_name
+@pytest.mark.parametrize("group_name", [None, "", "None"])
+def test_should_show_group_button_missing_group_name(group_name):
+    assert not gg.should_show_group_button(group_name, "")
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+def test_should_show_group_button_only_item_in_group(mock_get_file_contents):
+    mock_get_file_contents.return_value = {"group name": GROUP_NAME, "items": [ITEM_NAME]}
+    assert not gg.should_show_group_button(GROUP_NAME, ITEM_NAME)
+
+
+@patch(f"{FILE_LOC}.get_file_contents")
+def test_should_show_group_button_true(mock_get_file_contents):
+    mock_get_file_contents.return_value = {"group name": GROUP_NAME, "items": [ITEM_NAME, "Item 2"]}
+    assert gg.should_show_group_button(GROUP_NAME, ITEM_NAME)
