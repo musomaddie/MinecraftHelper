3e72b384b78e431b4634d0cf92f4c6fde3acf4f3
Maddie Wagner <musomaddy@gmail.com>
2023-03-23 20:28:32 +1100

    removing unused variables.

--- test_populate_info/conftest.py
+++ test_populate_info/conftest.py
@@ -6,33 +6,12 @@ import pytest
 import populate_info.resources as r
 from populate_info import create_app
 
-EXISTING_GROUP = "Test Group"
-MISSING_GROUP = "Missing Group"
-# TODO: delete the above and only use the below.
 GROUP_1 = "Test Group 1"
 GROUP_2 = "Test Group 2"
 GROUP_3 = "Test Group 3"
 ITEM_1 = "Test Item 1"
 ITEM_2 = "Test Item 2"
 ITEM_3 = "Test Item 3"
-
-
-def _wipe_added_blocks():
-    with open(r.ADDED_ITEM_FN, "w") as f:
-        json.dump({r.ITEM_LIST_KEY: []}, f)
-
-
-def _wipe_existing_group():
-    with open(r.get_group_fn(EXISTING_GROUP), "w") as f:
-        json.dump({
-            r.GROUP_NAME_KEY: EXISTING_GROUP,
-            r.GROUP_ITEMS_KEY: ["Existing Item"]
-        }, f)
-
-
-def _wipe_all():
-    _wipe_added_blocks()
-    _wipe_existing_group()
 
 
 def _create_file(filename, data):
@@ -82,21 +61,3 @@ def create_tmp_dir(monkeypatch, tmp_path):
     monkeypatch.chdir(tmp_path)
     _create_file(r.ADDED_ITEM_FN, {r.ITEM_LIST_KEY: []})
     _create_file(r.FULL_ITEMS_LIST_FN, {r.ITEM_LIST_KEY: [ITEM_1, ITEM_2, ITEM_3]})
-
-# @pytest.fixture(autouse=True)
-# def change_directory(monkeypatch):
-#     # TODO: delete this once I have switched to fully using tmp files.
-#     monkeypatch.chdir("testing_resources/")
-
-#
-# @pytest.fixture(autouse=True)
-# def teardown():
-#     # TODO: delete this once I have switched to using temp files fully!
-#     yield
-#     _wipe_all()
-#     # Get group name.
-#     try:
-#         os.remove(r.get_group_fn(GROUP_1))
-#     except FileNotFoundError:
-#         # As we're cleaning up we don't care if these files don't exist.
-#         pass
