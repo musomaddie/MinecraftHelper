dc9357f804a6bf72e8bc89bc61b29f0e3d5f20b5
Madeleine Wagner <musomaddy@gmail.com>
2023-03-25 07:12:40 +1100

    Breaking population html fully populated, and group toggle allowed.

--- populate_info/population_pages/breaking_population.py
+++ populate_info/population_pages/breaking_population.py
@@ -1,22 +1,21 @@
-from flask import render_template, session
+from flask import redirect, render_template, request, session, url_for
 
 import populate_info.resources as r
+from populate_info.group_utils import maybe_group_toggle_update_saved
 from populate_info.population_pages import item_blueprint
 
 
-@item_blueprint.route("/breaking/<item_name>", methods=["GET"])
+@item_blueprint.route("/breaking/<item_name>", methods=["GET", "POST"])
 def breaking(item_name):
     """ Handles populating the breaking obtainment method."""
-    return render_template(
-        "add_item/breaking.html",
-        item_name=item_name,
-        is_toggle_selected=session.get(r.USE_GROUP_VALUES_SK, False)
-    )
+    if request.method == "GET":
+        return render_template(
+            "add_item/breaking.html",
+            item_name=item_name,
+            group_name=session[r.GROUP_NAME_SK],
+            is_toggle_selected=session.get(r.USE_GROUP_VALUES_SK, False),
+            # TODO - temporarily on for testing!
+            show_group=True)
 
-# item_name = item_name,
-# item_url = r.get_item_url(item_name),
-# group_name = group_name,
-# is_toggle_selected = session.get(r.USE_GROUP_VALUES_SK, True),
-# group_categories = r.category_names_to_html_ids(get_group_categories(group_name)),
-# # Temporarily on for testing TODO
-# show_group = True)
+    if maybe_group_toggle_update_saved(session, request.form):
+        return redirect(url_for("add.breaking", item_name=item_name))
