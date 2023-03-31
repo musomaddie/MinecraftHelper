45aa48e0e1ae583713ed7457e93d8360d41eceee
Maddie Wagner <musomaddy@gmail.com>
2023-04-01 07:33:43 +1100

    Crafting html with basic data.

--- populate_info/population_pages/crafting_population.py
+++ populate_info/population_pages/crafting_population.py
@@ -1,6 +1,12 @@
+from flask import render_template
+
 from populate_info.population_pages import item_blueprint
 
 
 @item_blueprint.route("/crafting/<item_name>", methods=["GET", "POST"])
 def crafting(item_name):
-    return "I am crafting"
+    return render_template(
+        "add_item/crafting.html", item_name=item_name,
+        # TODO - temporarily on
+        is_toggle_selected=True,
+        show_group=True)
