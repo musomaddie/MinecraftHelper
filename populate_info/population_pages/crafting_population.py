c471706bcafccd4fe1483b871bc436010d0377db
Maddie Wagner <musomaddy@gmail.com>
2023-04-01 12:54:49 +1100

    Begin handling behaviour crafting.

--- populate_info/population_pages/crafting_population.py
+++ populate_info/population_pages/crafting_population.py
@@ -1,12 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
 
+from populate_info.group_utils import maybe_group_toggle_update_saved
 from populate_info.population_pages import item_blueprint
 
 
 @item_blueprint.route("/crafting/<item_name>", methods=["GET", "POST"])
 def crafting(item_name):
-    return render_template(
-        "add_item/crafting.html", item_name=item_name,
-        # TODO - temporarily on
-        is_toggle_selected=True,
-        show_group=True)
+    """ Handles populating the crafting obtainment method. """
+    if request.method == "GET":
+        return render_template(
+            "add_item/crafting.html",
+            item_name=item_name,
+            # TODO - temporarily on
+            is_toggle_selected=True,
+            show_group=True)
+
+    if maybe_group_toggle_update_saved(session, request.form):
+        return redirect(url_for("add.crafting", item_name=item_name))
+
+    # Remaining steps from here:
+    # 1. Retrieve the data from the form.
+    # 2. Save the data to JSON. (item and group).
+    # 3. Return possibliy move on.
